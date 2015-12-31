6ccb1f3c36147abcaa69041d3bdd640a7c272e74
Sufyaan Kazi <skazi@pivotal.io>
2015-12-31 13:04:24 +0000

    Corrected detection of previous route

--- promotion.sh
+++ promotion.sh
@@ -25,31 +25,41 @@ trap 'abort' 0
 echo_msg "Performing Build"
 ./gradlew build 
 APPNAME=cities
-DATE=`date "+%Y%m%d%H%M%S"`
-APPNAME=$APPNAME-$DATE
 
 cf target
 
 echo_msg "Detecting Original Route"
-OLD_ROUTES=`cf apps | grep cities | cut -d"," -f2 | cut -d"." -f1`
-for OLD_ROUTE in ${OLD_ROUTES[@]}
+OLD_ROUTES=`cf apps | grep $APPNAME | cut -d"," -f2 | cut -d"." -f1`
+for A_ROUTE in ${OLD_ROUTES[@]}
 do
-  OLD_ROUTE=`echo $OLD_ROUTE | xargs`
-  if [[ $OLD_ROUTE =~ [0-9] ]]; then
-      echo "$OLD_ROUTE contains numbers"
-  else
-      echo "gotcha: $OLD_ROUTE"
+  if [[ $A_ROUTE != *$APPNAME* ]]; then
+      continue
+  fi
+  A_ROUTE=`echo $A_ROUTE | xargs`
+  if ! [[ $A_ROUTE =~ [0-9] ]]; then
+      OLD_ROUTE=$A_ROUTE
       break
+    else
+      OLD_ROUTE="" 
   fi
 done
+echo "OLD ROUTE IS $OLD_ROUTE"
+
+DATE=`date "+%Y%m%d%H%M%S"`
+APPNAME=$APPNAME-$DATE
 
 #Push the new version
 echo_msg "Pushing new App Version"
+#If old route was blnak - need to create one by setting random_route - but 
+#we will ignore this for now as this script is usally run in controlled manner where there will be a route
 cf push $APPNAME -b java_buildpack_offline
 DOMAIN=`cf target | grep "API" | cut -d" " -f5 | sed "s/[^.]*.//"`
 cf map-route $APPNAME $DOMAIN -n $OLD_ROUTE
 echo_msg "Route mapped to old and new version of Application"
 cf apps
+
+echo "Temp sleep in script for demo purposes only........."
+sleep 10
 
 #Remove the old version
 echo_msg "Removing previous versions"

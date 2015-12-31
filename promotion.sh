28578056053b058f9ed67e333355ade200e9cd8e
Sufyaan Kazi <skazi@pivotal.io>
2015-12-31 14:07:09 +0000

    Corrected Promotion

--- promotion.sh
+++ promotion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 set -e 
+# set -vx
 
 abort()
 {
@@ -25,11 +26,20 @@ trap 'abort' 0
 echo_msg "Performing Build"
 ./gradlew build 
 APPNAME=cities
+BASE_APP=$APPNAME
 
 cf target
+cf apps
 
 echo_msg "Detecting Original Route"
-OLD_ROUTES=`cf apps | grep $APPNAME | cut -d"," -f2 | cut -d"." -f1`
+# Fetch current apps and
+# 1) Filter out ones with our app name
+# 2) Removing trailing spaces
+# 3) replace the space character in the separator between route names
+# 4) Grab the last part of string (i.e. just the routes of the apps)
+# 5) Split multiple routes into separate lines
+# 6) Remove the domain
+OLD_ROUTES=`cf apps | grep $APPNAME | sed -e 's/[[:space:]]*$//' | sed "s/, /,/" | rev | cut -d" " -f1 | rev | tr , '\n' | cut -d "." -f1`
 for A_ROUTE in ${OLD_ROUTES[@]}
 do
   if [[ $A_ROUTE != *$APPNAME* ]]; then
@@ -44,14 +54,16 @@ do
   fi
 done
 echo "OLD ROUTE IS $OLD_ROUTE"
+if [ ! "$OLD-ROUTE" ]; then
+  echo "Could not determine previous route!!"
+  exit 1
+fi
 
 DATE=`date "+%Y%m%d%H%M%S"`
 APPNAME=$APPNAME-$DATE
 
 #Push the new version
 echo_msg "Pushing new App Version"
-#If old route was blnak - need to create one by setting random_route - but 
-#we will ignore this for now as this script is usally run in controlled manner where there will be a route
 cf push $APPNAME -b java_buildpack_offline
 DOMAIN=`cf target | grep "API" | cut -d" " -f5 | sed "s/[^.]*.//"`
 cf map-route $APPNAME $DOMAIN -n $OLD_ROUTE
@@ -63,13 +75,14 @@ sleep 10
 
 #Remove the old version
 echo_msg "Removing previous versions"
-APPS_TO_DELETE=`cf apps | grep $OLD_ROUTE | cut -d" " -f1 | grep -v $APPNAME`
+APPS_TO_DELETE=`cf apps | grep $BASE_APP | cut -d" " -f1 | grep -v $APPNAME`
 for APP_TO_DELETE in ${APPS_TO_DELETE[@]}
 do
   cf delete -f $APP_TO_DELETE
 done
 echo_msg "Removing orphaned routes"
 cf delete-orphaned-routes -f
+cf apps
 
 trap : 0
 
